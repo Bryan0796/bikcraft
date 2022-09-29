b9003bbaa51cfbdbf6694c63f598dd5d3d4ad0c6
Bryan de Palma Martins Vida <bpmv96@outlook.com>
2022-09-29 18:30:37 -0300

    menu-ativo e descricao na pag de orcament-js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// ativar links do menu
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const element = document.getElementById(parametro);
+  if (element) {
+  element.checked = true;
+  }
+}
+
+
+parametros.forEach(ativarProduto);
+
+console.log(parametros);
